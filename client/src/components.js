3efa695913ee2304ff503a30704ec3d9f7a599ce
fdAssis <fd07ass@gmail.com>
2021-05-21 00:20:07 -0300

    initial setup of the terminal screen

--- client/src/components.js
+++ client/src/components.js
@@ -1,1 +1,77 @@
-import 
+import blassed from 'blessed';
+
+export default class ComponentsBuilder {
+  #screen;
+  #layout;
+  #input;
+
+  constructor() {}
+
+  #baseComponent() {
+    return {
+      border: 'line',
+      mouse: true,
+      keys: true,
+      top: 0,
+      scrollboar: {
+        ch: ' ',
+        inverse: true,
+      },
+
+      //habilita colocar cores e tags no texto
+      tags: true,
+    };
+  }
+
+  setScreen({ title }) {
+    this.#screen = blassed.screen({
+      smartCSR: true,
+      title,
+    });
+
+    this.#screen.key(['escape', 'q', 'C-c'], () => process.exit(0));
+
+    return this;
+  }
+
+  setLayoutComponent() {
+    this.#layout = blassed.layout({
+      parent: this.#screen,
+      width: '100%',
+      height: '100%',
+    });
+
+    return this;
+  }
+
+  setInputComponent(onEnterPressed) {
+    const input = blassed.textarea({
+      parent: this.#screen,
+      bottom: 0,
+      height: '10%',
+      inputOnFocus: true,
+      padding: {
+        top: 1,
+        left: 2,
+      },
+      style: {
+        fg: '#f6f6f6',
+        bg: '#353535',
+      },
+    });
+
+    input.key('enter', onEnterPressed);
+    this.#input = input;
+
+    return this;
+  }
+
+  build() {
+    const components = {
+      screen: this.#screen,
+      input: this.#input,
+    };
+
+    return components;
+  }
+}
--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,7 +1,24 @@
+import ComponentsBuilder from './components.js';
+
 export default class TerminalController {
   constructor() {}
 
+  #onInputRecevied(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+      console.log(message);
+      this.clearValue();
+    };
+  }
+
   async itializeTable(eventEmitter) {
-    console.log('Inicializoul');
+    const components = new ComponentsBuilder()
+      .setScreen({ title: 'TeaChat' })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputRecevied(eventEmitter))
+      .build();
+
+    components.input.focus();
+    components.screen.render();
   }
 }
