e69dbdec44a94c2d38d37bce1e767558c2d36bab
fdAssis <fd07ass@gmail.com>
2021-05-22 16:59:55 -0300

    cliConfig

--- client/index.js
+++ client/index.js
@@ -1,5 +1,10 @@
 import Events from 'events';
 import TerminalController from './src/terminalController.js';
+import CliConfig from './src/cliConfig.js';
+
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
+console.log({ config });
 
 const componentEmitter = new Events();
 
