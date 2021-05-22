0debe2c82b662e45d18e1c43540a775546b71c98
fdAssis <fd07ass@gmail.com>
2021-05-22 18:00:26 -0300

    constants and test connection

--- server/src/socket.js
+++ server/src/socket.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import { v4 } from 'uuid';
+import { constants } from './constants.js';
 
 export default class SocketServer {
   constructor({ port }) {
@@ -24,6 +25,7 @@ export default class SocketServer {
         .join('');
 
       socket.write(headers);
+      eventEmitter.emit(constants.event.NEW_USER_CONNECTED, socket);
     });
 
     return new Promise((resolve, reject) => {
