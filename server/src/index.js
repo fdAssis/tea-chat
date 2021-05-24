95b73d4a6077f959b564b9b2cf50cce1dbcf6b22
fdAssis <fd07ass@gmail.com>
2021-05-24 14:59:57 -0300

    socket client

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import SocketServer from './socket.js';
 import Event from 'events';
 import { constants } from './constants.js';
-import controller from './controller.js';
+import Controller from './controller.js';
 
 const eventEmitter = new Event();
 
@@ -37,6 +37,7 @@ const port = process.env.PORT || 9898;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.initialize(eventEmitter);
 console.log('socket server is running at:', server.address().port);
+const controller = new Controller({ socketServer });
 
 eventEmitter.on(
   constants.event.NEW_USER_CONNECTED,
